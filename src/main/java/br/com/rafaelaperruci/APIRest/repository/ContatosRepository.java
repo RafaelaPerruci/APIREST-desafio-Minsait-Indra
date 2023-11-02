a858945abb118e73daf7e35571744ef7deab6c09
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-02 20:32:37 -0300

    adição das interfaces de repositório

---
+++ src/main/java/br/com/rafaelaperruci/APIRest/repository/ContatosRepository.java
@@ -0,0 +1,4 @@
+package br.com.rafaelaperruci.APIRest.repository;
+
+public interface ContatosRepository {
+}
---
+++ src/main/java/br/com/rafaelaperruci/APIRest/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package br.com.rafaelaperruci.APIRest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository; 
+import br.com.rafaelaperruci.APIRest.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+}
