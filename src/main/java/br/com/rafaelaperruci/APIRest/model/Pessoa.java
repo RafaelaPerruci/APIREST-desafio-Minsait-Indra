fdece47de2bbfecb5869ae30acd3e1ba9d9e87dd
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-02 18:20:39 -0300

    adição do autoincremento da chave primária

--- src/main/java/br/com/rafaelaperruci/APIRest/model/Pessoa.java
+++ src/main/java/br/com/rafaelaperruci/APIRest/model/Pessoa.java
@@ -3,12 +3,19 @@ package br.com.rafaelaperruci.APIRest.model;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity 
 @Table (name = "pessoa")
 public class Pessoa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String nome;
     private String endereco;
     private String cep;
