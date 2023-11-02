2442f3b6f2542edcb54462df4cd2f1f0cff36514
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-02 17:57:09 -0300

    Adição do jpa e criação das entitys

--- src/main/java/br/com/rafaelaperruci/APIRest/model/Contatos.java
+++ src/main/java/br/com/rafaelaperruci/APIRest/model/Contatos.java
@@ -1,5 +1,62 @@
 package br.com.rafaelaperruci.APIRest.model;
 
+import java.util.Objects;
+
 public class Contatos {
-    
+    private long id;
+    private int tipoContato;
+    private String contato;
+    private Pessoa pessoa;
+
+    public Contatos(long id, int tipoContato, String contato, Pessoa pessoa) {
+        this.id = id;
+        this.tipoContato = tipoContato;
+        this.contato = contato;
+        this.pessoa = pessoa;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getTipoContato() {
+        return tipoContato;
+    }
+
+    public void setTipoContato(int tipoContato) {
+        this.tipoContato = tipoContato;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contatos contatos)) return false;
+        return getId() == contatos.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
 }
--- src/main/java/br/com/rafaelaperruci/APIRest/model/Pessoa.java
+++ src/main/java/br/com/rafaelaperruci/APIRest/model/Pessoa.java
@@ -2,8 +2,10 @@ package br.com.rafaelaperruci.APIRest.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 
-public class Pessoa { 
+@Entity
+public class Pessoa {
     private long id;
     private String nome;
     private String endereco;
@@ -20,108 +22,65 @@ public class Pessoa {
         this.cidade = cidade;
         this.uf = uf;
     }
-    
 
- 
-    @Override 
-    public int hashCode(){
-        return Objects.hash(id);
-    }
 
-    @Override
-    public boolean equals(Object obj){
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Pessoa other = (Pessoa) obj; 
-        return Objects.equals(id, other.id);           
-    }
-
-    /**
-     * @return long return the id
-     */
     public long getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(long id) {
         this.id = id;
     }
 
-    /**
-     * @return String return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return String return the endereco
-     */
     public String getEndereco() {
         return endereco;
     }
 
-    /**
-     * @param endereco the endereco to set
-     */
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
-    /**
-     * @return String return the cep
-     */
     public String getCep() {
         return cep;
     }
 
-    /**
-     * @param cep the cep to set
-     */
     public void setCep(String cep) {
         this.cep = cep;
     }
 
-    /**
-     * @return String return the cidade
-     */
     public String getCidade() {
         return cidade;
     }
 
-    /**
-     * @param cidade the cidade to set
-     */
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
-    /**
-     * @return String return the uf
-     */
     public String getUf() {
         return uf;
     }
 
-    /**
-     * @param uf the uf to set
-     */
     public void setUf(String uf) {
         this.uf = uf;
     }
 
-}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa pessoa)) return false;
+        return getId() == pessoa.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+} 
