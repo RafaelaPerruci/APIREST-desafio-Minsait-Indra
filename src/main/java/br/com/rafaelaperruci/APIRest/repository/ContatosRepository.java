2b8ac57ecd078c1d3e78c12ec7a5db49e9cf6a9b
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-03 08:03:14 -0300

    criação das interfaces

--- src/main/java/br/com/rafaelaperruci/APIRest/repository/ContatosRepository.java
+++ src/main/java/br/com/rafaelaperruci/APIRest/repository/ContatosRepository.java
@@ -1,7 +1,18 @@
 package br.com.rafaelaperruci.APIRest.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.com.rafaelaperruci.APIRest.model.Contatos;
+import java.util.List;
+ 
+
 
 public interface ContatosRepository extends JpaRepository<Contatos, Long>{
+    Contatos save(Contatos contatos); 
+    Optional<Contatos> getById(long id); 
+    List<Contatos> getAll(); 
+    Contatos update (Contatos contatos);
+    void delete(Long id);
 }
