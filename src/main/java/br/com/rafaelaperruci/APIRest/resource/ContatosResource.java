d0d265a63176f3f84f44359cdd18ae26a5a7edc5
Rafaela Perruci <rafaelaoperruci@gmail.com>
2023-11-03 16:47:19 -0300

    criação das controllers

---
+++ src/main/java/br/com/rafaelaperruci/APIRest/resource/ContatosResource.java
@@ -0,0 +1,4 @@
+package br.com.rafaelaperruci.APIRest.resource;
+
+public class ContatosResource {
+}
---
+++ src/main/java/br/com/rafaelaperruci/APIRest/resource/PessoaResource.java
@@ -0,0 +1,30 @@
+package br.com.rafaelaperruci.APIRest.resource;
+
+import br.com.rafaelaperruci.APIRest.model.Pessoa;
+import br.com.rafaelaperruci.APIRest.service.interfaces.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pessoa")
+public class PessoaResource {
+    private PessoaService pessoaService;
+
+    @Autowired
+    public PessoaResource(PessoaService pessoaService){
+        this.pessoaService = pessoaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> getAllPessoa() {
+        List<Pessoa> pessoas = pessoaService.getAll();
+        if (pessoas == null)
+            return ResponseEntity.notFound().build();
+        return  ResponseEntity.ok(pessoas);
+    }
+}
